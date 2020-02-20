2c9fe84b377f6d3d89498f5f04314b2f5e09a92b
lzp <15321168632@163.com>
2020-02-20 15:26:14 +0800

    fix

---
+++ src/main/java/com/roncoo/eshop/cache/config/GlobalConfig.java
@@ -0,0 +1,53 @@
+package com.roncoo.eshop.cache.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+public class GlobalConfig {
+
+    /**
+     * mysql
+     *
+     * @return
+     */
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return new org.apache.tomcat.jdbc.pool.DataSource();
+//    }
+//
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+//        sqlSessionFactoryBean.setDataSource(dataSource());
+//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:/mybatis/*.xml"));
+//        return sqlSessionFactoryBean.getObject();
+//    }
+//
+//    @Bean
+//    public PlatformTransactionManager transactionManager() {
+//        return new DataSourceTransactionManager(dataSource());
+//    }
+
+    /**
+     * redis
+     *
+     * @return
+     */
+    @Bean
+    public JedisCluster JedisClusterFactory() {
+        Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+//        jedisClusterNodes.add(new HostAndPort("192.168.198.130", 7001));
+//        jedisClusterNodes.add(new HostAndPort("192.168.198.131", 7003));
+//        jedisClusterNodes.add(new HostAndPort("192.168.198.132", 7005));
+        JedisCluster jedisCluster = new JedisCluster(jedisClusterNodes);
+        return jedisCluster;
+    }
+}
