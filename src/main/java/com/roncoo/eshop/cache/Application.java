56bf176f0de5b7730e6a072c5253bd5afc331c77
lzp <15321168632@163.com>
2020-02-28 00:59:46 +0800

    fix

--- src/main/java/com/roncoo/eshop/cache/Application.java
+++ src/main/java/com/roncoo/eshop/cache/Application.java
@@ -2,10 +2,12 @@ package com.roncoo.eshop.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@ServletComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
