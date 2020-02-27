dba6ca511a1439dd6e998fb382bb4f9f7002bd8b
lzp <15321168632@163.com>
2020-02-28 00:06:21 +0800

    fix

--- src/main/java/com/roncoo/eshop/cache/Application.java
+++ src/main/java/com/roncoo/eshop/cache/Application.java
@@ -1,15 +1,11 @@
 package com.roncoo.eshop.cache;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-@ServletComponentScan
-@MapperScan("com.roncoo.eshop.cache.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
